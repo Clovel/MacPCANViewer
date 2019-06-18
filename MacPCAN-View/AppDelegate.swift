357b9685a5decc65feec7a16faedd29755d58a5f
Clovis Durand <cd.clovel19@gmail.com>
2019-06-18 16:25:25 +0200

    Added XCode Project + Started UI dev

---
+++ MacPCAN-View/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacPCAN-View
+//
+//  Created by Clovis Durand on 18/06/2019.
+//  Copyright © 2019 Clovis Durand. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacPCAN-View/MessagesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MacPCAN-View
+//
+//  Created by Clovis Durand on 18/06/2019.
+//  Copyright © 2019 Clovis Durand. All rights reserved.
+//
+
+import Cocoa
+
+class MessagesViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        // self.view.setFrameSize(NSSize(width: 640, height: 360))
+        
+        print("Hello world !")
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
