2b6f41ad6e4e5b6124bac1b351dcb669cd1d86fd
Clovis Durand <cd.clovel19@gmail.com>
2019-06-19 11:10:35 +0200

    Updated CANDriver.cpp

--- src/CANDriver.cpp
+++ src/CANDriver.cpp
@@ -10,8 +10,54 @@
 
 #include <iostream>
 
-namespace PCBUSB {
-    #include <PCBUSB.h>
+#include <PCBUSB.h>
+
+// Defines ------------------------------------------------
+#define PCBUSB_LOCAL_NEUTRAL 0x00
+#define PCBUSB_LOCAL_GERMAN 0x07
+#define PCBUSB_LOCAL_ENGLISH 0x09
+#define PCBUSB_LOCAL_SPANISH 0x0A
+#define PCBUSB_LOCAL_ITALIAN 0x10
+#define PCBUSB_LOCAL_FRENCH 0x1C
+
+static void canMsgToPCBUSB(const CAN::can_msg_t &in, TPCANMsg &out) {
+    out.ID = in.msg_id;
+    out.LEN = in.data_sz;
+    for(uint8_t i = 0U; out.LEN; ++i) {
+        out.DATA[i] = in.data[i];
+    }
+    out.MSGTYPE = in.flags;
+}
+
+static void canMsgFromPCBUSB(const TPCANMsg &in, CAN::can_msg_t &out) {
+    out.msg_id = in.ID;
+    out.data_sz = in.LEN;
+    for(uint8_t i = 0U; out.data_sz; ++i) {
+        out.data[i] = in.DATA[i];
+    }
+    out.flags = in.MSGTYPE; /* TODO : Check if this is true */
+}
+
+static void canTimeStampToPCBUSB(const CAN::can_rx_timestamp_t &in, TPCANTimestamp &out) {
+    out.millis = in.milliseconds;
+    out.millis_overflow = in.milliseconds_overflow;
+    out.micros = in.microseconds;
+}
+
+static void canTimeStampFromPCBUSB(const TPCANTimestamp &in, CAN::can_rx_timestamp_t &out) {
+    out.milliseconds = in.millis;
+    out.milliseconds_overflow = in.millis_overflow;
+    out.microseconds = in.micros;
+}
+
+static uint16_t getErrorText(const TPCANStatus &pStatus, std::string &pErrorText) {
+    char lBuffer[1024U]; /* TODO : Find a real value and define it */
+    uint32_t lStatus = CAN_GetErrorText(pStatus, PCBUSB_LOCAL_ENGLISH, lBuffer);
+    
+    pErrorText.erase();
+    pErrorText = std::string(lBuffer);
+
+    return lStatus;
 }
 
 namespace CAN {
@@ -20,11 +66,13 @@ namespace CAN {
     }
     
     CANDriver::~CANDriver() {
-        unsigned long lStatus = PCBUSB::CAN_Uninitialize(mChannel);
+        uint32_t lStatus = CAN_Uninitialize(mChannel);
         
         /* TODO : ERROR message */
         if(0U < lStatus) {
-            std::cout << "[ERROR] <CANDriver::init> CAN_Uninitialize failed : Unknown error occured !" << std::endl;
+            std::string lErrorText;
+            (void)getErrorText(lStatus, lErrorText);
+            std::cout << "[ERROR] <CANDriver::destructor> CAN_Uninitialize failed : " << lErrorText << std::endl;
         }
         
         mChannel = 0U;
@@ -36,22 +84,22 @@ namespace CAN {
         return sInstance;
     }
     
-    int CANDriver::init(const unsigned short &pChannel) {
-        unsigned long lStatus = PCBUSB::CAN_Initialize(pChannel, PCAN_BAUD_1M);
-        
-        if(PCAN_ERROR_NODRIVER == (PCAN_ERROR_NODRIVER & lStatus)) {
-            std::cout << "[ERROR] <CANDriver::init> CAN_Initialize failed : No CAN driver is loaded !" << std::endl;
-            return 1;
-        } else if(0U < lStatus) {
-            std::cout << "[ERROR] <CANDriver::init> CAN_Initialize failed : Unknown error occured !" << std::endl;
-            return 255;
+    uint32_t CANDriver::init(const unsigned short &pChannel) {
+        unsigned long lStatus = CAN_Initialize(pChannel, PCAN_BAUD_1M);
+        std::string lErrorMsg;
+
+        if(0U < lStatus) {
+            std::string lErrorText;
+            (void)getErrorText(lStatus, lErrorText);
+            std::cout << "[ERROR] <CANDriver::init> CAN_Initialize failed : " << lErrorText << std::endl;
+            return lStatus;
         }
         
         /* TODO : Cover the other error cases */
         
         mChannel = pChannel;
         
-        return 0;
+        return lStatus;
     }
     
     /* Getters */
@@ -66,13 +114,40 @@ namespace CAN {
     /* Setters */
     
     /* CAN bus manipulation */
-    int CANDriver::send(const can_msg_t &pMsg) {
-        //
-        return 0;
+    uint32_t CANDriver::send(const can_msg_t &pMsg) {
+        TPCANMsg lMsg;
+        canMsgToPCBUSB(pMsg, lMsg);
+
+        uint32_t lStatus = CAN_Write(mChannel, &lMsg);
+
+        if(0U < lStatus) {
+            std::string lErrorText;
+            (void)getErrorText(lStatus, lErrorText);
+            std::cout << "[ERROR] <CANDriver::send> CAN_Initialize failed : " << lErrorText << std::endl;
+            //ERROR
+            return lStatus;
+        }
+
+        return lStatus;
     }
     
-    int CANDriver::read(can_msg_t &pMsg) {
-        //
-        return 0;
+    uint32_t CANDriver::read(can_msg_t &pMsg, can_rx_timestamp_t &pTimeStamp) {
+        TPCANMsg lMsg;
+        TPCANTimestamp lTS;
+
+        uint32_t lStatus = CAN_Read(mChannel, &lMsg, &lTS);
+
+        if(0U < lStatus) {
+            std::string lErrorText;
+            (void)getErrorText(lStatus, lErrorText);
+            std::cout << "[ERROR] <CANDriver::read> CAN_Read failed : " << lErrorText << std::endl;
+            //ERROR
+            return lStatus;
+        }
+
+        canMsgFromPCBUSB(lMsg, pMsg);
+        canTimeStampFromPCBUSB(lTS, pTimeStamp);
+
+        return lStatus;
     }
 }
