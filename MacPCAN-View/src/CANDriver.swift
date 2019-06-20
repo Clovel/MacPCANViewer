d405874231ee6d1b02dd89fcef36435a1c82c4d6
Clovis Durand <cd.clovel19@gmail.com>
2019-06-20 16:31:03 +0200

    Updated CANDriver

--- MacPCAN-View/src/CANDriver.swift
+++ MacPCAN-View/src/CANDriver.swift
@@ -9,15 +9,154 @@
 import Foundation
 
 enum CANChannels: UInt16 {
-    case CHANNEL_NONE = 0
-    case CHANNEL_USBBUS1 = 1
-    case CHANNEL_USBBUS2 = 2
-    case CHANNEL_USBBUS3 = 3
-    case CHANNEL_USBBUS4 = 4
-    case CHANNEL_USBBUS5 = 5
-    case CHANNEL_USBBUS6 = 6
-    case CHANNEL_USBBUS7 = 7
-    case CHANNEL_USBBUS8 = 8
+    case CHANNEL_NONE = 0x00
+    case CHANNEL_USBBUS1 = 0x51
+    case CHANNEL_USBBUS2 = 0x52
+    case CHANNEL_USBBUS3 = 0x53
+    case CHANNEL_USBBUS4 = 0x54
+    case CHANNEL_USBBUS5 = 0x55
+    case CHANNEL_USBBUS6 = 0x56
+    case CHANNEL_USBBUS7 = 0x57
+    case CHANNEL_USBBUS8 = 0x58
+}
+
+enum CANBusBaudrates: UInt16 {
+    case CAN_BR_1M     = 0x0014
+    case CAN_BR_800K = 0x0016
+    case CAN_BR_500K = 0x001C
+    case CAN_BR_250K = 0x011C
+    case CAN_BR_125K = 0x031C
+    case CAN_BR_100K = 0x432F
+    case CAN_BR_95K  = 0xC34E
+    case CAN_BR_83K  = 0x852B
+    case CAN_BR_50K  = 0x472F
+    case CAN_BR_47K  = 0x1414
+    case CAN_BR_33K  = 0x8B2F
+    case CAN_BR_20K  = 0x532F
+    case CAN_BR_10K  = 0x672F
+    case CAN_BR_5K   = 0x7F7F
+}
+
+func canChannelFromString(_ pStr: String) -> CANChannels {
+    switch pStr {
+    case "CHANNEL_NONE":
+        return CANChannels.CHANNEL_NONE
+    case "CHANNEL_USBBUS1":
+        return CANChannels.CHANNEL_USBBUS1
+    case "CHANNEL_USBBUS2":
+        return CANChannels.CHANNEL_USBBUS2
+    case "CHANNEL_USBBUS3":
+        return CANChannels.CHANNEL_USBBUS3
+    case "CHANNEL_USBBUS4":
+        return CANChannels.CHANNEL_USBBUS4
+    case "CHANNEL_USBBUS5":
+        return CANChannels.CHANNEL_USBBUS5
+    case "CHANNEL_USBBUS6":
+        return CANChannels.CHANNEL_USBBUS6
+    case "CHANNEL_USBBUS7":
+        return CANChannels.CHANNEL_USBBUS7
+    case "CHANNEL_USBBUS8":
+        return CANChannels.CHANNEL_USBBUS8
+    default:
+        return CANChannels.CHANNEL_NONE
+    }
+}
+
+func canChannelToString(_ pChannel: CANChannels) -> String {
+    switch pChannel {
+    case CANChannels.CHANNEL_NONE:
+        return "CHANNEL_NONE"
+    case CANChannels.CHANNEL_USBBUS1:
+        return "CHANNEL_USBBUS1"
+    case CANChannels.CHANNEL_USBBUS2:
+        return "CHANNEL_USBBUS2"
+    case CANChannels.CHANNEL_USBBUS3:
+        return "CHANNEL_USBBUS3"
+    case CANChannels.CHANNEL_USBBUS4:
+        return "CHANNEL_USBBUS4"
+    case CANChannels.CHANNEL_USBBUS5:
+        return "CHANNEL_USBBUS5"
+    case CANChannels.CHANNEL_USBBUS6:
+        return "CHANNEL_USBBUS6"
+    case CANChannels.CHANNEL_USBBUS7:
+        return "CHANNEL_USBBUS7"
+    case CANChannels.CHANNEL_USBBUS8:
+        return "CHANNEL_USBBUS8"
+//    default: /* Will never be executed, commented to avoid warning */
+//        return "CHANNEL_NONE"
+    }
+}
+
+func canBaudrateFromString(_ pStr: String) -> CANBusBaudrates {
+    switch pStr {
+    case "CAN_BR_1M":
+        return CANBusBaudrates.CAN_BR_1M
+    case "CAN_BR_800K":
+        return CANBusBaudrates.CAN_BR_800K
+    case "CAN_BR_500K":
+        return CANBusBaudrates.CAN_BR_500K
+    case "CAN_BR_250K":
+        return CANBusBaudrates.CAN_BR_250K
+    case "CAN_BR_125K":
+        return CANBusBaudrates.CAN_BR_125K
+    case "CAN_BR_100K":
+        return CANBusBaudrates.CAN_BR_100K
+    case "CAN_BR_95K":
+        return CANBusBaudrates.CAN_BR_95K
+    case "CAN_BR_83K":
+        return CANBusBaudrates.CAN_BR_83K
+    case "CAN_BR_50K":
+        return CANBusBaudrates.CAN_BR_50K
+    case "CAN_BR_47K":
+        return CANBusBaudrates.CAN_BR_47K
+    case "CAN_BR_33K":
+        return CANBusBaudrates.CAN_BR_33K
+    case "CAN_BR_20K":
+        return CANBusBaudrates.CAN_BR_20K
+    case "CAN_BR_10K":
+        return CANBusBaudrates.CAN_BR_10K
+    case "CAN_BR_5K":
+        return CANBusBaudrates.CAN_BR_5K
+    default:
+        /* 1M is default value */
+        return CANBusBaudrates.CAN_BR_1M
+    }
+}
+
+func canBaudrateToString(_ pBr: CANBusBaudrates) -> String {
+    switch pBr {
+    case CANBusBaudrates.CAN_BR_1M:
+        return "CAN_BR_1M"
+    case CANBusBaudrates.CAN_BR_800K:
+        return "CAN_BR_800K"
+    case CANBusBaudrates.CAN_BR_500K:
+        return "CAN_BR_500K"
+    case CANBusBaudrates.CAN_BR_250K:
+        return "CAN_BR_250K"
+    case CANBusBaudrates.CAN_BR_125K:
+        return "CAN_BR_125K"
+    case CANBusBaudrates.CAN_BR_100K:
+        return "CAN_BR_100K"
+    case CANBusBaudrates.CAN_BR_95K:
+        return "CAN_BR_95K"
+    case CANBusBaudrates.CAN_BR_83K:
+        return "CAN_BR_83K"
+    case CANBusBaudrates.CAN_BR_50K:
+        return "CAN_BR_50K"
+    case CANBusBaudrates.CAN_BR_47K:
+        return "CAN_BR_47K"
+    case CANBusBaudrates.CAN_BR_33K:
+        return "CAN_BR_33K"
+    case CANBusBaudrates.CAN_BR_20K:
+        return "CAN_BR_20K"
+    case CANBusBaudrates.CAN_BR_10K:
+        return "CAN_BR_10K"
+    case CANBusBaudrates.CAN_BR_5K:
+        return "CAN_BR_5K"
+//    default: /* Will never be executed, commented to avoid warning */
+//        /* 1M is default value */
+//        return "CAN_BR_1M"
+    }
 }
 
 enum CANDriverLocales: UInt16 {
@@ -29,25 +168,18 @@ enum CANDriverLocales: UInt16 {
     case PCBUSB_LOCAL_FRENCH  = 0x1C
 }
 
-fileprivate func getErrorText(pStatus: UInt) -> String {
-    let lBufferPtr: UnsafeMutablePointer<Int8> = UnsafeMutablePointer<Int8>.allocate(capacity: 1024)
-    
-    let _: UInt = CAN_GetErrorText(pStatus, CANDriverLocales.PCBUSB_LOCAL_ENGLISH.rawValue, lBufferPtr)
-    
-    let lStr: String = String(cString: lBufferPtr)
-    print("[DEBUG] <CANDriver.swift::getErrorText> lStr = " + lStr)
-    
-    return lStr
-}
-
 class CANDriver {
     private var mChannel: CANChannels
     
-    private static var shared: CANDriver = {() -> CANDriver in
+    private static var sShared: CANDriver = {() -> CANDriver in
         let lDriver: CANDriver = CANDriver()
         
         return lDriver
     }()
+    
+    public class func instance() -> CANDriver {
+        return sShared
+    }
     
     /* constructor is private, this is a singleton */
     private init() {
@@ -62,10 +194,21 @@ class CANDriver {
         return mChannel.rawValue
     }
     
-    public func initDriver(pChannel: CANChannels, pErrorStr: inout String) -> UInt {
-        let lStatus: UInt = CAN_Initialize(pChannel.rawValue, UInt16(PCAN_BAUD_1M), UInt8(PCAN_USB), 0, 0) /* Args 3, 4, 5 are not used w/ PCAN-USB */
+    private func getErrorText(_ pStatus: UInt) -> String {
+        let lBufferPtr: UnsafeMutablePointer<Int8> = UnsafeMutablePointer<Int8>.allocate(capacity: 1024)
+        
+        let _: UInt = CAN_GetErrorText(pStatus, CANDriverLocales.PCBUSB_LOCAL_ENGLISH.rawValue, lBufferPtr)
+        
+        let lStr: String = String(cString: lBufferPtr)
+        print("[DEBUG] <CANDriver.swift::getErrorText> lStr = " + lStr + " (status = " + String(format:"0x%04X", pStatus) + ")")
+        
+        return lStr
+    }
+    
+    public func initDriver(_ pChannel: CANChannels, _ pBaudrate: CANBusBaudrates, _ pErrorStr: inout String) -> UInt {
+        let lStatus: UInt = CAN_Initialize(pChannel.rawValue, pBaudrate.rawValue, UInt8(PCAN_USB), 0, 0) /* Args 3, 4, 5 are not used w/ PCAN-USB */
         if(0 < lStatus) {
-            pErrorStr = getErrorText(pStatus: lStatus)
+            pErrorStr = getErrorText(lStatus)
             return lStatus
         }
         
@@ -75,25 +218,43 @@ class CANDriver {
         return lStatus
     }
     
-    public func send(pMsg: TPCANMsg, pErrorStr: inout String) -> UInt {
+    public func send(_ pMsg: TPCANMsg, _ pErrorStr: inout String) -> UInt {
         var lMsg: TPCANMsg = pMsg /* Copy to avoid having to put inout to pMsg */
         let lPtr: UnsafeMutablePointer<TPCANMsg> = withUnsafeMutablePointer(to: &lMsg) { $0 }
         
         let lStatus: UInt = CAN_Write(mChannel.rawValue, lPtr)
         if(0 < lStatus) {
-            pErrorStr = getErrorText(pStatus: lStatus)
+            pErrorStr = getErrorText(lStatus)
         }
         
         return lStatus
     }
     
-    public func read(pMsg: inout TPCANMsg, pTimeStamp: inout TPCANTimestamp, pErrorStr: inout String) -> UInt {
+    public func read(_ pMsg: inout TPCANMsg, _ pTimeStamp: inout TPCANTimestamp, _ pErrorStr: inout String) -> UInt {
         let lMsgPtr: UnsafeMutablePointer<TPCANMsg>      = withUnsafeMutablePointer(to: &pMsg) { $0 }
         let lTSPtr: UnsafeMutablePointer<TPCANTimestamp> = withUnsafeMutablePointer(to: &pTimeStamp) { $0 }
         
         let lStatus: UInt = CAN_Read(mChannel.rawValue, lMsgPtr, lTSPtr)
         if(0 < lStatus) {
-            pErrorStr = getErrorText(pStatus: lStatus)
+            pErrorStr = getErrorText(lStatus)
+        }
+        
+        return lStatus
+    }
+    
+    public func status(_ pErrorStr: inout String) -> UInt {
+        let lStatus: UInt = CAN_GetStatus(mChannel.rawValue)
+        if(0 < lStatus) {
+            pErrorStr = getErrorText(lStatus)
+        }
+        
+        return lStatus
+    }
+    
+    public func resetQueues(_ pErrorStr: inout String) -> UInt {
+        let lStatus: UInt = CAN_Reset(mChannel.rawValue)
+        if(0 < lStatus) {
+            pErrorStr = getErrorText(lStatus)
         }
         
         return lStatus
