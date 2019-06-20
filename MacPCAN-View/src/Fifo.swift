1dc287aaaf4d0964a8b7394d01ebd82c6c234fdb
Clovis Durand <cd.clovel19@gmail.com>
2019-06-20 16:30:15 +0200

    Added Fifo class

---
+++ MacPCAN-View/src/Fifo.swift
@@ -0,0 +1,53 @@
+//
+//  Fifo.swift
+//  MacPCAN-View
+//
+//  Created by Clovis Durand on 19/06/2019.
+//  Copyright © 2019 Clovis Durand. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Note : This is inspired by :
+ * https://github.com/raywenderlich/swift-algorithm-club/tree/master/Queue
+ */
+
+public class Fifo<T> {
+    fileprivate var mArray = [T]()
+    fileprivate var mCapacity: Int
+    
+    public init(pCapacity: Int) {
+        mCapacity = pCapacity
+    }
+    
+    public var isEmpty: Bool {
+        return mArray.isEmpty
+    }
+    
+    public var count: Int {
+        return mArray.count
+    }
+    
+    /* this function allows the user to take a peak at the first element */
+    public var front: T? {
+        return mArray.first
+    }
+    
+    public func put(_ pElement: T) -> Bool {
+        if(mCapacity == count) {
+            return false /* FIFO is full ! */
+        } else {
+            mArray.append(pElement)
+            return true
+        }
+    }
+    
+    public func get() -> T? {
+        if(isEmpty) {
+            return nil
+        } else {
+            return mArray.removeFirst()
+        }
+    }
+}
